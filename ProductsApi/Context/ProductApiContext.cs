746ca22c0835c4b729ab68e8ce3679170effe5a3
Guilherme Luiz Da Silva <36992072854@info.local>
2024-08-07 15:09:43 -0300

    productApi

---
+++ ProductsApi/Context/ProductApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAPI.Domains;
+
+namespace ProductsAPI.Context
+{
+    public class ProductsAPIContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Data Source=DESKTOP-MHF127S; initial catalog=ProductsApi_GuiLuiz_tarde; user Id= sa; pwd=Senai@134; TrustServerCertificate=true;");
+    }
+}
---
+++ ProductsApi/Migrations/20240807180202_ProductsApi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductsApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    IdProduct = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(40)", nullable: true),
+                    Price = table.Column<decimal>(type: "DECIMAL(10,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.IdProduct);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
